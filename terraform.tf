bc8273a06cee2f478a7b4426a474a995ceb32bc3
muthurarvind <94586791+muthurarvind@users.noreply.github.com>
2022-01-31 17:42:34 +0530

    Update terraform.tf

--- terraform.tf
+++ terraform.tf
@@ -9,7 +9,7 @@ provider "aws" {
 	  key_name = "sshkey1"
 	  user_data = templatefile("${path.module}/userdata.tftpl", {endpoint = aws_db_instance.default.endpoint,address = aws_db_instance.default.address})
 	  iam_instance_profile = "demo"
-	  security_groups = ["sg-052b7d954440d8def"]
+	  security_groups = ["sg-0222697dff90cafd9"]
 	  tags = {
 	    Name = "cpms"
 	  }
@@ -26,5 +26,5 @@ provider "aws" {
 	  parameter_group_name = "default.mysql5.7"
 	  skip_final_snapshot  = true
 	  publicly_accessible  = true
-	  vpc_security_group_ids = ["sg-052b7d954440d8def"]
+	  vpc_security_group_ids = ["sg-0222697dff90cafd9"]
 	}
